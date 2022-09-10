0dd97986e7c1aee6f7dcd6f088117b7dda73000b
IfunanyaWilliam <williamifunanya@gmail.com>
2022-09-11 00:13:22 +0100

    Add PostsController

---
+++ CwkSocial.Api/Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using Cwk.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PostsController : Controller
+    {
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id, Text = "Hello, World" };
+            return Ok(post);
+        }
+    }
+}
